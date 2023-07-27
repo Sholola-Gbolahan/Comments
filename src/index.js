625d717e9afeab78eac2609f761ebef8de7d6eb0
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2023-07-27 08:56:55 +0100

    coomment created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom/client';
+import { faker } from '@faker-js/faker'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const App = () => {
+    return (
+      <div className='ui container comments'>
+        <div className='comment'>
+          <a href='/' className='avatar'>
+            <img alt='Avatar' src={faker.image.avatar()}/>
+          </a>
+          <div className='content'>
+            <a href='/' className='author'>Sam</a>
+            <div className='metadata'>
+              <span>Today at 6:00pm </span>
+            </div>
+            <div className='text'>Nice blog post!</div>   
+          </div>
+          
+        </div>
+
+        <div className='comment'>
+          <a href='/' className='avatar'>
+            <img alt='Avatar' src={faker.image.avatar()}/>
+          </a>
+          <div className='content'>
+            <a href='/' className='author'>Sam</a>
+            <div className='metadata'>
+              <span>Today at 6:00pm </span>
+            </div>
+            <div className='text'>Nice blog post!</div>   
+          </div>
+          
+        </div>
+        
+      </div>
+
+            );
+
+}
+
+const root = ReactDom.createRoot(document.querySelector('#root'))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
