1d886e2189f8a0fed4931f2f98d2ee10d2d46a3c
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2023-07-31 14:10:01 +0100

    Approval Card Component Created

--- src/CommentDetails.jsx
+++ src/CommentDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CommentDetails = (props) => {
-    console.log(props);
+    // console.log(props);
   return (
     <div>
          <div className='comment'>
--- src/index.js
+++ src/index.js
@@ -2,19 +2,32 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import Comment from './CommentDetails';
 import { faker } from '@faker-js/faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
-      <div className='ui container comments'>
+      <>
+        <div className='ui container comments'>
 
-       <Comment
-        author ='Sam' 
-        timeAgo='Today at 2:30pm'
-        feedback='Nice blog post'
-        avatar={faker.image.avatar()}/>
-       <Comment author ='Daniel' timeAgo='Yesterday at 4:00pm'  feedback='Kinda of cool though' avatar={faker.image.avatar()}  />
-       <Comment author='Samson' timeAgo='Today at 2:30pm'  feedback='I feel you bro, keep it up' avatar={faker.image.avatar()}/>
-      </div>
+          <ApprovalCard >
+            <Comment author ='SamR' timeAgo='Today at 2:30pm' feedback='Nice blog post' avatar={faker.image.avatar()}/>
+          </ApprovalCard>
+
+          <ApprovalCard>
+           <Comment author ='Daniel' timeAgo='Yesterday at 4:00pm'  feedback='Kinda of cool though' avatar={faker.image.avatar()} />
+          </ApprovalCard>
+          
+          <ApprovalCard>
+            <Comment author='Samson' timeAgo='Today at 2:30pm'  feedback='I feel you bro, keep it up' avatar={faker.image.avatar()}/> 
+          </ApprovalCard>
+
+          
+           
+
+        </div>
+       
+      </>
+      
 
             );
 
